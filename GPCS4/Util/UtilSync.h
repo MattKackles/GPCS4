b7885dabd8a1e0bd5c68f921108d006ed69ad579
MattKackles <punkfacewally@gmail.com>
2021-05-19 00:05:36 -0400

    Add files via upload

--- GPCS4/Util/UtilSync.h
+++ GPCS4/Util/UtilSync.h
@@ -4,9 +4,6 @@
 #include <atomic>
 #include <thread>
 
-namespace sync
-{;
-
 /**
  * \brief Spin lock
  *
@@ -14,41 +11,63 @@ namespace sync
  * protect data structures for a short duration
  * in case the structure is not likely contested.
  */
-class Spinlock
-{
+
+// detect x86
+#ifdef _M_X64
+#define __UTIL_SYNC_X86__ 1
+#elif _M_IX86
+#define __UTIL_SYNC_X86__ 1
+#elif __x86_64
+#define __UTIL_SYNC_X86__ 1
+#elif __i386
+#define __UTIL_SYNC_X86__ 1
+#else
+// none of the above
+#define __UTIL_SYNC_UNKNOWN_ARCH__ 1
+#endif
+
+#ifdef __UTIL_SYNC_UNKNOWN_ARCH__
+#error "unsupported platform"
+#endif
+
+#ifdef __UTIL_SYNC_X86__
+//https://software.intel.com/content/www/us/en/develop/articles/benefitting-power-and-performance-sleep-loops.html
+//https://www.felixcloutier.com/x86/pause
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+void pause() {
+#ifdef _MSC_VER
+    _mm_pause();
+#else
+    __builtin_ia32_pause();
+#endif
+}
+#endif
+
+class Spinlock {
 
 public:
-	Spinlock()
-	{
-	}
-	~Spinlock()
-	{
-	}
+    Spinlock() = default;
+    Spinlock(const Spinlock &) = delete;
+    Spinlock &operator=(const Spinlock &) = delete;
 
-	Spinlock(const Spinlock&) = delete;
-	Spinlock& operator=(const Spinlock&) = delete;
+    void lock() {
+        uint32_t expected;
+        while (!m_lock.compare_exchange_weak(expected = 0, 1, std::memory_order_release, std::memory_order_consume)) {
+            pause();
+        }
+    }
 
-	void lock()
-	{
-		while (!this->try_lock())
-		{
-			std::this_thread::yield();
-		}
-	}
+    void unlock() {
+        m_lock.store(0, std::memory_order_release);
+    }
 
-	void unlock()
-	{
-		m_lock.store(0, std::memory_order_release);
-	}
-
-	bool try_lock()
-	{
-		return !m_lock.load() && !m_lock.exchange(1, std::memory_order_acquire);
-	}
+    bool try_lock() {
+        uint32_t expected = 0;
+        return m_lock.compare_exchange_strong(expected, 1, std::memory_order_release, std::memory_order_consume);
+    }
 
 private:
-	std::atomic<uint32_t> m_lock = { 0 };
+    std::atomic<uint32_t> m_lock{};
 };
-
-}  // namespace sync
-
